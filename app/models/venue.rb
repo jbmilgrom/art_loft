7a11e1431000229c913bc9a56cc20a552cc01f70
jbmilgrom <jbmilgrom@gmail.com>
2014-05-19 14:27:35 -0400

    notes

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,14 +2,14 @@ class Venue < ActiveRecord::Base
 	belongs_to :user
 	has_many :events
 
-	def self.create(user_id, address, name=nil, url)
-      self.create({
-        user_id: user_id, 
-        address: address,
-        name: name,
-        url: url 
-        })
-      User.find(user_id).has_venue = true
-  	end
+	# def self.create(user_id, address, name=nil, url)
+ #      self.create({
+ #        user_id: user_id, 
+ #        address: address,
+ #        name: name,
+ #        url: url 
+ #        })
+ #      User.find(user_id).has_venue = true
+ #  	end
 
 end
