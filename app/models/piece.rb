95895f260ec03164ed1f4d9fe9496a94ecb5787d
jbmilgrom <jbmilgrom@gmail.com>
2014-05-19 14:30:46 -0400

    notes

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,13 +4,19 @@ class Piece < ActiveRecord::Base
 	has_many :events, through: :exhibitions
   	has_many :exhibitions
 
-  def self.create(user_id, url)
-      self.create({
-        user_id: user_id, 
-        url: url 
-        })
-      User.find(user_id).is_artist = true
-  end
+  # def self.create(user_id, url)
+  #     self.create({
+  #       user_id: user_id, 
+  #       url: url 
+  #       })
+  #     User.find(user_id).is_artist = true
+  # end
+
+
+
+
+
+  
   # 	 def add_portion(quantity, unit_name, ingredient_name)
   #   #
   #   ingredient = Ingredient.find_by_name(ingredient_name)
