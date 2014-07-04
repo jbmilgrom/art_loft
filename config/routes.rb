12643ed882907c3c323e7cca8fd0cfe934430ccf
jbmilgrom <jbmilgrom@gmail.com>
2014-07-04 08:37:53 -0400

    removed superfluous code in routes and application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  get "/sign_up" => "users#new", :as => "sign_up"
+
+  get "/log_out" => "sessions#destroy", :as => "log_out"
+  get "/log_in" => "sessions#new", :as => "log_in"
+  resources :sessions, except: [:new, :destroy]
+
   # resources :pieces
 
   #get "/artists/:id" => "artists#show"
@@ -10,43 +16,17 @@ Rails.application.routes.draw do
   #get "/hosts/:id" => "hosts#show"
   resources :hosts, only: [:show]
 
-  get "/sign_up" => "users#new", :as => "sign_up"
+
   resources :users do
     resources :pieces
   end
 
 
-  get "/log_out" => "sessions#destroy", :as => "log_out"
-  get "/log_in" => "sessions#new", :as => "log_in"
-  resources :sessions
-  # post "/pieces" => "pieces#create"
 
-  # get "/pieces/:id" => "pieces#show"
-
-  # get "/pieces/:id/edit" => "pieces#edit"
-
-
-  # get "/hosts" => "hosts#index"  
-
-  # post "/events" => "events#create"
-
-  
-  # get "/venues" => "venues#index"
 
   post "/venues" => "venues#create"
 
-  # get "/venues/:id" => "venues#show"
-
-  # get "/venues/:id/edit" => "venues#edit"
-
 
   resources :events
-  # get "/events" => "events#index"  
-
-  # # post "/events" => "events#create"
-
-  # get "/events/:id" => "events#show"
-
-  # get "/events/:id/edit" => "events#edit"
 
 end
