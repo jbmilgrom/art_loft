11f745c517a7763846a84d109f8537d72f25f130
jbmilgrom <jbmilgrom@gmail.com>
2014-08-18 10:14:46 -0400

    fixing tabbing bug

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,22 +1,22 @@
 class VenuesController < ApplicationController
 
-	def index
-	end 
+  def index
+  end 
 
-	def create
-		Venue.create(venue_attributes)
-		user_id = venue_attributes[:user_id]
-		user = User.find(user_id)
-		user.has_venue = true
-		user.save
+  def create
+    Venue.create(venue_attributes)
+    user_id = venue_attributes[:user_id]
+    user = User.find(user_id)
+    user.has_venue = true
+    user.save
 
-		redirect_to "/users/#{user_id}"
-	end
+    redirect_to "/users/#{user_id}"
+  end
 
-	private
+  private
 
-  	def venue_attributes
-    	params.require(:venue).permit(:address, :url, :user_id)
-  	end
+  def venue_attributes
+    params.require(:venue).permit(:address, :url, :user_id)
+  end
 
 end
