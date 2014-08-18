be0939331b8f728c29881a70759bb41411e40112
jbmilgrom <jbmilgrom@gmail.com>
2014-08-18 10:07:00 -0400

    fixing tabbing bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,24 @@
 class EventsController < ApplicationController
 
-	def index
+  def index
+    @events = Event.order(:start_time)
+  end 
 
-		@events = Event.order(:start_time)
-	
-	end 
+  def show 
+    id = params[:id]
+    @event = Event.find(id)
+    @host = @event.host
+    @artists = @event.artists
+  end
 
-	def show 
-		id = params[:id]
-		@event = Event.find(id)
-		@host = @event.host
-		@artists = @event.artists
-	end
+  def new
+    @event = Event.new
+  end
 
-	def new
-		@event = Event.new
-	end
+  private
 
-	private
-
-  	def user_attributes
-    	params.require(:event).permit(:f_name, :l_name, :email, :password, :profile_pic, :user_id)
-  	end
+  def user_attributes
+    params.require(:event).permit(:f_name, :l_name, :email, :password, :profile_pic, :user_id)
+  end
 
 end
